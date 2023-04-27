0df885d650c4eeed5c8f7cf4a60fc0c0130db5c4
Thomas <mosset.thomas@gmail.com>
2023-04-27 15:39:59 +0200

    feat: replace tailwind default pagination style for bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        // allow to use Bootstrap style instead of Tailwind for pagination
+        Paginator::useBootstrapFive();
     }
 }
