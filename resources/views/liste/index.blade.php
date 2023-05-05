e9213877cefdc1e6295eaf58322646ef47ac5c2e
Thomas <mosset.thomas@gmail.com>
2023-05-05 19:53:03 +0200

    style: added class to list of word + pagination

--- resources/views/liste/index.blade.php
+++ resources/views/liste/index.blade.php
@@ -3,33 +3,28 @@
 @section('title', 'Accueil')
 
 @section('content')
-    <h1 class="text-center">Liste de mots rares</h1>
+
+<div class="mx-auto my-5 p-3 d-flex flex-column">
+    <h1 class="text-center text-success mb-5">Liste de mots rares</h1>
 
     @foreach ($words as $word)
-        <div>
-            <div>
-                <h2>{{ $word->word }}</h2>
-
-                @if ($word->pronunciation !== null)
-                    <span>(Se prononce : '{{ $word->pronunciation }}')</span>
-                @endif
+        <div class="my-4 p-3 d-flex justify-content-between align-items-center list-single-word">
+            <div class="d-flex flex-column">
+                <h2 class="text-success">{{ $word->word }}</h2>
+                <span class="fw-bold">[{{ $word->type }}]</span>
             </div>
-            
-            <span>{{ $word->type }}</span>
 
             <p>{{ $word->definition }}</p>
 
-            @if ($word->exemple !== null)
-                <p>Ex: {{ $word->exemple }}</p>
-            @endif
-
-            <a href="{{ route('liste.showOne', ['slug' => $word->slug, 'id' => $word->id]) }}" class="btn btn-success">Accéder au mot</a>
-
+            <div class="btn-action-div">
+                <a href="{{ route('liste.showOne', ['slug' => $word->slug, 'id' => $word->id]) }}" class="btn btn-success">Accéder au mot</a>
+            </div>
         </div>
     @endforeach
+</div>
 
     <!-- for pagination-->
-    <div class="d-flex justify-content-center">
+    <div class="pagination-container d-flex justify-content-center">
         {{ $words->links() }}
     </div>
 @endsection
